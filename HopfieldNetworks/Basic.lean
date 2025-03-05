4cae6f030b94f087592b55b9b27af774423453c7
Matteo Cipollina <matteo.cipollina@yahoo.it>
2025-03-05 19:26:49 +0100

    Update Basic.lean

--- HopfieldNetworks/Basic.lean
+++ HopfieldNetworks/Basic.lean
@@ -2,6 +2,8 @@ import Mathlib.LinearAlgebra.Matrix.BilinearForm
 import Mathlib.LinearAlgebra.Matrix.Hermitian
 import Mathlib.LinearAlgebra.QuadraticForm.Basic
 
+variable {n : ℕ}
+
 
 /-!
 # Hopfield Networks Formalization
