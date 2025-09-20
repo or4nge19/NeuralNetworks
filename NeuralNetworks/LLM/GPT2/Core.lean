74dc92df9429f2ce19629027579ae38549c31a78
or4nge19 <matteo.cipollina@yahoo.it>
2025-09-21 00:11:11 +0200

    fixes

--- NeuralNetworks/LLM/GPT2/Core.lean
+++ NeuralNetworks/LLM/GPT2/Core.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Matteo Cipollina. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Matteo Cipollina, Alok Singh
+-/
+
 import Batteries.Data.ByteArray
 import Init.Data.Array.Bootstrap
 import Batteries.Data.Fin.Basic
--- NeuralNetworks/LLM/GPT2/Model'.lean
+++ NeuralNetworks/LLM/GPT2/Model'.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Matteo Cipollina. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Matteo Cipollina, Alok Singh
+-/
+
 import NeuralNetworks.LLM.GPT2.Core
 import NeuralNetworks.LLM.GPT2.TensorView.Lemmas
 
